7ae25f26af8ccf9f321d0af6b22f4b3ba8ecdbed
dmnitprof <dmn.itprof@gmail.com>
2020-10-15 01:39:52 +0300

    add 1

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
 	return (<>
 		<h1>About Page</h1>
-		<button><a>Вернуться на главную!!!</a></button>
+		<button><a>Вернуться на главную!!</a></button>
 	</>
 )
 }
