3c7957732d7b6739031715b93b4d7e4de1c20b0d
dmnitprof <dmn.itprof@gmail.com>
2020-10-15 00:46:34 +0300

    edit about.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+
 import Link from 'next/link'
+import React from 'react'
 
 export default function Index() {
-	return <>
-		<h1>Hello Next js</h1>
-		<p><Link href={'/about'}><a>About</a></Link></p>
-		<p><Link href={'/posts'}><a>Posts</a></Link></p>
-		<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
-	</>
+	return (
+		<>
+			<h1>Hello Next js<h1 />
+				<p><Link href={'/about'}><a>About</a></Link></p>
+				<p><Link href={'/posts'}><a>Posts</a></Link></p>
+				<p>Lorem Ipsum - это текст- рыба, часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной рыбой для текстов на латинице с начала XVI века.</p>
+			</h1>
+		</>
+	)
 }
